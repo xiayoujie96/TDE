373ae8e683cda7b2cbba75bb586fc49248ca46fe
xiayoujie96 <zhixingheyi96@gmail.com>
2024-08-01 15:24:28 -0300

    tde6.py

--- tde6.py
+++ tde6.py
@@ -1,3 +1,4 @@
+import math
 #2⋅π⋅raio(altura+raio) eh como calcula a area do cilindro
 #entradas
 lataValor=50
@@ -5,9 +6,11 @@ lataPinta=3
 alturaCilindro=float(input("Insira a altura do cilindro:"))
 raioCilindro=float(input("Insira o raio do cilindro:"))
 #processamento
-calculoAreaM2=2*3.1415*raioCilindro*(alturaCilindro+raioCilindro)
+calculoAreaM2=2*math.pi*raioCilindro*(alturaCilindro+raioCilindro)
 quantidadeLatas = calculoAreaM2/lataPinta
 valorTotal = quantidadeLatas*lataValor
+num = calculoAreaM2
+rounded_num = round(num, 3)
 #saida
-print("A area total do cilindro é de:",calculoAreaM2)
-print("serao necessarias:", quantidadeLatas, "latas e isso custará:", valorTotal, "reais")
+print("A area total do cilindro é de:",rounded_num)
+print(f"serao necessarias: {quantidadeLatas:.2f}", f"latas e isso custará: {valorTotal:.2f} reais")
