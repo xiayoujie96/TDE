a0501366f1406c2a5454ef7cf9b0d55ffc743ec3
xiayoujie96 <zhixingheyi96@gmail.com>
2024-08-06 15:36:51 -0300

    Update tde011.py

---
+++ tde011.py
@@ -0,0 +1,6 @@
+idade = int(input())
+if 5 <= idade <= 7: print('Infantil A')
+elif 8 <= idade <= 10: print('Infantil B')
+elif 11 <= idade <= 13: print('Juvenil A')
+elif 14 <= idade <= 17: print('Juvenil B')
+elif idade > 17: print ('Adulto')
